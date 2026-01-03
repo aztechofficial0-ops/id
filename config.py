fec3e5ab9b279cd5a9082043a7352c0e843c5988
aztechofficial0-ops <aztechofficial0@gmail.com>
2026-01-03 22:42:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,19 +2,19 @@ import os
 from typing import Dict, List, Optional, TypedDict
 
 # Telegram Bot token
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8542265590:AAE5JEjA6-N9cfkR1il2aIkyByh6EDUWRqc")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8568564305:AAGK3PcjHEMVlyb1tOB6Fibhj1GzQHsspos")
 
 # Mongo
 MONGO_URI = os.getenv(
     "MONGO_URI",
-    "mongodb+srv://aztech:ayazahmed1122@cluster0.mhuaw3q.mongodb.net/idstore_db?retryWrites=true&w=majority",
+    "mongodb+srv://aztech:ayazahmed1122@cluster0.mhuaw3q.mongodb.net/jarvisidstore_db?retryWrites=true&w=majority",
 )
-DB_NAME = os.getenv("DB_NAME", "idstore_db")
+DB_NAME = os.getenv("DB_NAME", "jarvisidstore_db")
 
 # Admin Telegram user IDs (comma-separated)
 ADMIN_USER_IDS: List[int] = [
     int(x)
-    for x in os.getenv("ADMIN_USER_IDS", "6670166083,").split(",")
+    for x in os.getenv("ADMIN_USER_IDS", "6670166083,7813102548").split(",")
     if x.strip().isdigit()
 ]
 
@@ -22,20 +22,20 @@ ADMIN_USER_IDS: List[int] = [
 START_IMAGE = "https://i.postimg.cc/zD73Wn61/photo-2025-12-28-18-55-27.jpg"
 
 # Bot username (without @) for referral links
-BOT_USERNAME = os.getenv("BOT_USERNAME", "DreamAccBot")
+BOT_USERNAME = os.getenv("BOT_USERNAME", "JarvisTgStoreBot")
 
 # Referral program percentage (3% forever)
 REFERRAL_PERCENT = float(os.getenv("REFERRAL_PERCENT", "3.0"))
 
 # Channel join requirement
-CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME", "DreamAccountSup")  # without @
+CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME", "JarvisTgStore")  # without @
 
 # Report channel (bot must be admin there). Without @
-REPORT_CHANNEL_USERNAME = os.getenv("REPORT_CHANNEL_USERNAME", "Dreamaccsreport")
+REPORT_CHANNEL_USERNAME = os.getenv("REPORT_CHANNEL_USERNAME", "JarvisSolds")
 
 # Fixed Telegram API credentials used for adding accounts (admin flow)
-TELEGRAM_API_ID = int(os.getenv("TELEGRAM_API_ID", "31538472"))
-TELEGRAM_API_HASH = os.getenv("TELEGRAM_API_HASH", "fa66f05927f292f94ce8d0034ca57242")
+TELEGRAM_API_ID = int(os.getenv("TELEGRAM_API_ID", "33428535"))
+TELEGRAM_API_HASH = os.getenv("TELEGRAM_API_HASH", "c0dbd6f2553e9ed7ab51db2c6cd3360e")
 
 
 # ----------------------------
@@ -66,19 +66,12 @@ class CryptoNetworkCfg(TypedDict, total=False):
 INR_QRS: Dict[str, PaymentMethodCfg] = {
     "qr1": {
         "label": "INR QR 1",
-        "payee_name": "HRIDAY NARAIN JAISWAL",
-        "upi_id": "BHARATPE09914513765@yesbankltd",
-        "notes": "REGARDS :- @BrutodGIFT\n\nCHECK USERNAME BEFORE DEAL",
-        "image_url": "https://i.postimg.cc/FsDM6Sw-T/inr.jpg",
+        "payee_name": "ARSH ALI",
+        "upi_id": "xd.arsh@fam",
+        "notes": "REGARDS :- @Axcne\n\nCHECK USERNAME BEFORE DEAL",
+        "image_url": "https://i.ibb.co/ccdJHMjB/x.jpg",
     },
     "qr2": {
-        "label": "INR QR 2",
-        "payee_name": "MANSI JAISWAL",
-        "upi_id": "paytmqr13l2j6z2w1@paytm",
-        "notes": "REGARDS :- @BrutodGIFT\n\nCHECK USERNAME BEFORE DEAL",
-        "image_url": "https://i.postimg.cc/JnLgPsf3/inr2.jpg",
-    },
-    "qr3": {
         "label": "FamPay Auto QR",
         "payee_name": "MOHAMMED AYAZ AHMED",
         "upi_id": "ayazahmedmd@fam",
@@ -120,5 +113,4 @@ CRYPTO_NETWORKS: Dict[str, CryptoNetworkCfg] = {
         "image_url": "https://i.postimg.cc/VL8sw-XGf/binance.jpg",
     },
 }
-
 
